800713c8a15b79a157440ea519cc3f5a85ffcefe
FEvelychko <maxdevelopenko@gmail.com>
2015-10-12 23:53:09 +0300

    updating of game

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 //debugger;
 //    var tileContainer = document.getElementsByClassName('tile-container');
     var tileContainer = document.getElementById('tile-container');
-
     var defaultTemplate = [
         '<div class="tile tile-2 tile-position-1-1 tile-new"><div class="tile-inner">2</div></div>',
         '<div class="tile tile-2 tile-position-1-2 tile-new"><div class="tile-inner">2</div></div>',
@@ -42,9 +41,9 @@
     ];
 
 
-    var firstNumber,secondNumber = undefined;
+    var firstNumber = undefined;
+    var secondNumber = undefined;
     function start(){
-
         //debugger;
         var randomFirst = Math.floor((Math.random() * defaultTemplate.length - 1) + 1);
         var randomSecond = Math.floor((Math.random() * defaultTemplate.length - 1) + 1);
@@ -75,18 +74,27 @@
     };
 
     function moveUp(){
-        //debugger;
-        //debugger;
-        //console.log(tileContainer.children[0].className.split(" ")[2].slice(16,17));
-        //var kozel = tileContainer.children[0].className.split(" ")[2].slice(16,17);
-
-        //tileContainer.children[0].className.split(" ")[2].replace(tileContainer.children[0].className.split(" ")[2].slice(16,17), 1);
-        debugger;
         var row = tileContainer.children[0].className.split(" ")[2].slice(16,17);
         var col = tileContainer.children[0].className.split(" ")[2].slice(14,15);
         var value = tileContainer.children[0].className.split(" ")[1].slice(5,6);
-        if(row > 1 && col == 1 && value == 2){
-            tileContainer.children[0].className = "tile tile-2 tile-position-1-1 tile-new";
+        var row2 = tileContainer.children[1].className.split(" ")[2].slice(16,17);
+        var col2 = tileContainer.children[1].className.split(" ")[2].slice(14,15);
+        var value2 = tileContainer.children[1].className.split(" ")[1].slice(5,6);
+        /*if(row > 1 && col == 1 && value == 2){
+            if(tileContainer.children[1].className == "tile tile-2 tile-position-1-1 tile-new"){
+                tileContainer.children[0].className = "tile tile-2 tile-position-1-2 tile-new"
+            }
+            if(tileContainer.children[1].className == "tile tile-2 tile-position-1-2 tile-new"){
+                tileContainer.children[1].className == "tile tile-2 tile-position-1-1 tile-new"
+                tileContainer.children[0].className = "tile tile-2 tile-position-1-2 tile-new"
+            }
+            if(tileContainer.children[1].className == "tile tile-2 tile-position-1-3 tile-new"){
+                tileContainer.children[1].className == "tile tile-2 tile-position-1-1 tile-new"
+                tileContainer.children[0].className = "tile tile-2 tile-position-1-2 tile-new"
+            }
+            if(tileContainer.children[1].className == "tile tile-2 tile-position-1-1 tile-new"){
+                tileContainer.children[0].className = "tile tile-2 tile-position-1-2 tile-new"
+            }
         }
         if(row > 1 && col == 2 && value == 2){
             tileContainer.children[0].className = "tile tile-2 tile-position-2-1 tile-new";
@@ -97,27 +105,45 @@
         if(row > 1 && col == 4 && value == 2){
             tileContainer.children[0].className = "tile tile-2 tile-position-4-1 tile-new";
         }
-        if(row > 1 && col == 1 && value == 2){
-            tileContainer.children[0].className = "tile tile-2 tile-position-1-1 tile-new";
+        if(row > 1 && col == 1 && value == 4){
+            tileContainer.children[0].className = "tile tile-4 tile-position-1-1 tile-new";
         }
-        if(row > 1 && col == 2 && value == 2){
-            tileContainer.children[0].className = "tile tile-2 tile-position-2-1 tile-new";
+        if(row > 1 && col == 2 && value == 4){
+            tileContainer.children[0].className = "tile tile-4 tile-position-2-1 tile-new";
         }
-        if(row > 1 && col == 3 && value == 2){
-            tileContainer.children[0].className = "tile tile-2 tile-position-3-1 tile-new";
+        if(row > 1 && col == 3 && value == 4){
+            tileContainer.children[0].className = "tile tile-4 tile-position-3-1 tile-new";
         }
-        if(row > 1 && col == 4 && value == 2){
-            tileContainer.children[0].className = "tile tile-2 tile-position-4-1 tile-new";
+        if(row > 1 && col == 4 && value == 4){
+            tileContainer.children[0].className = "tile tile-4 tile-position-4-1 tile-new";
         }
-        console.log(tileContainer.children[0].className);
-        /*if(foundRow > 1){
-            foundRow = 1;
+
+        if(row2 > 1 && col2 == 1 && value2 == 2){
+            tileContainer.children[1].className = "tile tile-2 tile-position-1-1 tile-new";
+        }
+        if(row2 > 1 && col2 == 2 && value2 == 2){
+            tileContainer.children[1].className = "tile tile-2 tile-position-2-1 tile-new";
+        }
+        if(row2 > 1 && col2 == 3 && value2 == 2){
+            tileContainer.children[1].className = "tile tile-2 tile-position-3-1 tile-new";
+        }
+        if(row2 > 1 && col2 == 4 && value2 == 2){
+            tileContainer.children[1].className = "tile tile-2 tile-position-4-1 tile-new";
+        }
+        if(row2 > 1 && col2 == 1 && value2 == 4){
+            tileContainer.children[1].className = "tile tile-4 tile-position-1-1 tile-new";
+        }
+        if(row2 > 1 && col2 == 2 && value2 == 4){
+            tileContainer.children[1].className = "tile tile-4 tile-position-2-1 tile-new";
+        }
+        if(row2 > 1 && col2 == 3 && value2 == 4){
+            tileContainer.children[1].className = "tile tile-4 tile-position-3-1 tile-new";
+        }
+        if(row2 > 1 && col2 == 4 && value2 == 4){
+            tileContainer.children[1].className = "tile tile-4 tile-position-4-1 tile-new";
         }*/
-        //return tileContainer.children[0];
-        //if()
-        //if(tileContainer.children[0].className == "")
+        console.log(tileContainer.children[0].className);
     }
-    //moveUp();
 
     document.onkeydown = function(event){
         event.which = event.which || event.keyCode;

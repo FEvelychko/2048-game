75506aab41399cc770a14f369850313362645905
FEvelychko <maxdevelopenko@gmail.com>
2015-10-14 23:58:27 +0300

    fixing of starting game

--- app.js
+++ app.js
@@ -6,38 +6,38 @@
 //    var tileContainer = document.getElementsByClassName('tile-container');
     var tileContainer = document.getElementById('tile-container');
     var defaultTemplate = [
-        '<div class="tile tile-2 tile-position-1-1 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-1-2 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-1-3 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-1-4 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-2-1 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-2-2 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-2-3 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-2-4 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-3-1 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-3-2 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-3-3 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-3-4 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-4-1 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-4-2 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-4-3 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-2 tile-position-4-4 tile-new"><div class="tile-inner">2</div></div>',
-        '<div class="tile tile-4 tile-position-1-1 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-1-2 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-1-3 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-1-4 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-2-1 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-2-2 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-2-3 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-2-4 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-3-1 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-3-2 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-3-3 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-3-4 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-4-1 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-4-2 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-4-3 tile-new"><div class="tile-inner">4</div></div>',
-        '<div class="tile tile-4 tile-position-4-4 tile-new"><div class="tile-inner">4</div></div>'
+        '<div class="tile tile-2 tile-position-11 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-12 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-13 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-14 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-21 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-22 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-23 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-24 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-31 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-32 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-33 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-34 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-41 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-42 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-43 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-2 tile-position-44 tile-new"><div class="tile-inner">2</div></div>',
+        '<div class="tile tile-4 tile-position-11 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-12 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-13 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-14 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-21 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-22 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-23 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-24 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-31 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-32 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-33 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-34 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-41 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-42 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-43 tile-new"><div class="tile-inner">4</div></div>',
+        '<div class="tile tile-4 tile-position-44 tile-new"><div class="tile-inner">4</div></div>'
     ];
 
 
@@ -45,36 +45,58 @@
     var secondNumber = undefined;
     function start(){
         //debugger;
+        var arr = [];
         var randomFirst = Math.floor((Math.random() * defaultTemplate.length - 1) + 1);
         var randomSecond = Math.floor((Math.random() * defaultTemplate.length - 1) + 1);
-
         firstNumber = defaultTemplate[randomFirst];
+        secondNumber = defaultTemplate[randomSecond];
+        debugger;
+
+        if(firstNumber.slice(38,40) !== secondNumber.slice(38,40)){
+            tileContainer.innerHTML = firstNumber + secondNumber;
+        }
+        else{
+            for(var x = 0; x<defaultTemplate.length; x++){
+                if(defaultTemplate[x].slice(38,40) != firstNumber.slice(38,40)){
+                    arr.push(defaultTemplate[x]);
+                }
+            }
+            secondNumber = arr[Math.floor((Math.random() * defaultTemplate.length - 1) + 1)];
+            tileContainer.innerHTML = firstNumber + secondNumber;
+        }
+
+            //.slice(38,40)
+            //tileContainer.innerHTML = firstNumber + secondNumber;
+
+        /*firstNumber = defaultTemplate[randomFirst];
         secondNumber = defaultTemplate[randomSecond];
         if(firstNumber === secondNumber){
             if(randomFirst < 31 && randomFirst >= 15){
                 randomFirst -= 10;
                 firstNumber = defaultTemplate[randomFirst];
-                tileContainer[0].innerHTML = firstNumber + secondNumber;
+                tileContainer.innerHTML = firstNumber + secondNumber;
             }
             else if(randomFirst === 31){
                 randomFirst -= 20;
                 firstNumber = defaultTemplate[randomFirst];
-                tileContainer[0].innerHTML = firstNumber + secondNumber;
+                tileContainer.innerHTML = firstNumber + secondNumber;
             }
             else if(randomFirst === 1){
                 randomFirst += 10;
                 firstNumber = defaultTemplate[randomFirst];
-                tileContainer[0].innerHTML = firstNumber + secondNumber;
+                tileContainer.innerHTML = firstNumber + secondNumber;
             }
         }
         else{
             tileContainer.innerHTML = firstNumber + secondNumber;
-        }
+            console.log(tileContainer.children[0].className.split(" ")[2].slice(14,16));
+            console.log(tileContainer.children[1].className.split(" ")[2].slice(14,16));
+        }*/
 
     };
 
     function moveUp(){
-        var row = tileContainer.children[0].className.split(" ")[2].slice(16,17);
+        var row = tileContainer.children[0].className.split(" ")[2].slice(14,16);
         var col = tileContainer.children[0].className.split(" ")[2].slice(14,15);
         var value = tileContainer.children[0].className.split(" ")[1].slice(5,6);
         var row2 = tileContainer.children[1].className.split(" ")[2].slice(16,17);

5617dd2e138cd289b29c7f9bdf7408982633c2ee
FEvelychko <maxdevelopenko@gmail.com>
2015-10-16 23:48:50 +0300

    fixing of starting method

--- app.js
+++ app.js
@@ -44,43 +44,68 @@
     var firstNumber = undefined;
     var secondNumber = undefined;
     var thirdNumber = undefined;
+
     function start(){
-            //debugger;
-            var arr = [];
+            debugger;
+            var leftAfterFirst = [];
             var randomFirst = Math.floor((Math.random() * defaultTemplate.length - 1) + 1);
             var randomSecond = Math.floor((Math.random() * defaultTemplate.length - 1) + 1);
             firstNumber = defaultTemplate[randomFirst];
             secondNumber = defaultTemplate[randomSecond];
-            //debugger;
 
-            if(firstNumber.slice(38,40) !== secondNumber.slice(38,40)){
+        for(var x =0; x <defaultTemplate.length; x++){
+        }
+            tileContainer.innerHTML = firstNumber + secondNumber;
+
+
+
+        /*for(var x = 0; x<defaultTemplate.length; x++){
+            if(defaultTemplate[x] != firstNumber){
+                leftAfterFirstRandom.push(defaultTemplate[x]);
+            }
+        }*/
+            /*if(firstNumber.slice(38,40) !== secondNumber.slice(38,40)){
                 tileContainer.innerHTML = firstNumber + secondNumber;
             }
             else{
-                for(var x = 0; x<defaultTemplate.length; x++){
-                    if(defaultTemplate[x].slice(38,40) != firstNumber.slice(38,40)){
-                        arr.push(defaultTemplate[x]);
-                    }
-                }
-                secondNumber = arr[Math.floor((Math.random() * defaultTemplate.length - 1) + 1)];
+                secondNumber = leftAfterFirstRandom[Math.floor((Math.random() * defaultTemplate.length - 1) + 1)];
                 tileContainer.innerHTML = firstNumber + secondNumber;
-            }
+            }*/
 
         };
 
     function moveUp() {
         debugger;
         var arrCell = [];
-        var randomThird = Math.floor((Math.random() * defaultTemplate.length - 1) + 1);
-        thirdNumber = defaultTemplate[randomThird];
+        var randomThird = Math.floor((Math.random() * leftAfterFirstRandom.length - 1) + 1);
+        thirdNumber = leftAfterFirstRandom[randomThird];
 
-        for(var i = 0; i < defaultTemplate.length; i++){
-            if(defaultTemplate[i].slice(38,39) === firstNumber.slice(38,39) && defaultTemplate[i].slice(22,23) === firstNumber.slice(22,23)){
+        for(var i = 0; i < defaultTemplate.length; i++) {
+            if (defaultTemplate[i].slice(38, 39) === firstNumber.slice(38, 39) && defaultTemplate[i].slice(22, 23) === firstNumber.slice(22, 23)) {
                 arrCell.push(defaultTemplate[i]);
             }
         }
-
+        if(firstNumber.slice(39,40) < arrCell[1].slice(39,40)){
             tileContainer.innerHTML += thirdNumber;
+            return true;
+        }
+
+        else if(firstNumber.slice(39,40) < arrCell[2].slice(39,40)){
+            firstNumber = arrCell[0];
+            tileContainer.innerHTML = firstNumber;
+            tileContainer.innerHTML += thirdNumber;
+        }
+        else if(firstNumber.slice(39,40) < arrCell[3].slice(39,40)){
+            firstNumber = arrCell[0];
+            tileContainer.innerHTML = firstNumber;
+            tileContainer.innerHTML += thirdNumber;
+        }
+        else{
+            tileContainer.innerHTML += thirdNumber;
+        }
+
+
+        //tileContainer.innerHTML += thirdNumber;
         console.log(tileContainer.innerHTML);
     }
 

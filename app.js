e8ed153f2de7987976843a93c39ed05a3a53690c
FEvelychko <maxdevelopenko@gmail.com>
2015-10-18 23:56:44 +0300

    checking the current container values

--- app.js
+++ app.js
@@ -37,6 +37,40 @@
         '<div class="tile tile-4 tile-position-43 tile-new"><div class="tile-inner">4</div></div>',
         '<div class="tile tile-4 tile-position-44 tile-new"><div class="tile-inner">4</div></div>'
     ];
+    var defaultClasses = [
+        'tile tile-2 tile-position-11 tile-new',
+        'tile tile-2 tile-position-12 tile-new',
+        'tile tile-2 tile-position-13 tile-new',
+        'tile tile-2 tile-position-14 tile-new',
+        'tile tile-2 tile-position-21 tile-new',
+        'tile tile-2 tile-position-22 tile-new',
+        'tile tile-2 tile-position-23 tile-new',
+        'tile tile-2 tile-position-24 tile-new',
+        'tile tile-2 tile-position-31 tile-new',
+        'tile tile-2 tile-position-32 tile-new',
+        'tile tile-2 tile-position-33 tile-new',
+        'tile tile-2 tile-position-34 tile-new',
+        'tile tile-2 tile-position-41 tile-new',
+        'tile tile-2 tile-position-42 tile-new',
+        'tile tile-2 tile-position-43 tile-new',
+        'tile tile-2 tile-position-44 tile-new',
+        'tile tile-4 tile-position-11 tile-new',
+        'tile tile-4 tile-position-12 tile-new',
+        'tile tile-4 tile-position-13 tile-new',
+        'tile tile-4 tile-position-14 tile-new',
+        'tile tile-4 tile-position-21 tile-new',
+        'tile tile-4 tile-position-22 tile-new',
+        'tile tile-4 tile-position-23 tile-new',
+        'tile tile-4 tile-position-24 tile-new',
+        'tile tile-4 tile-position-31 tile-new',
+        'tile tile-4 tile-position-32 tile-new',
+        'tile tile-4 tile-position-33 tile-new',
+        'tile tile-4 tile-position-34 tile-new',
+        'tile tile-4 tile-position-41 tile-new',
+        'tile tile-4 tile-position-42 tile-new',
+        'tile tile-4 tile-position-43 tile-new',
+        'tile tile-4 tile-position-44 tile-new'
+    ];
 
 
     var firstItem = undefined; // змінні що були створені, але не були про ініціалізовані а null змінні що були очищенні до певного місця знчення
@@ -70,9 +104,14 @@
     }
 
     function moveUp() {
-        debugger;
+        //debugger;
         var randomNext = Math.floor((Math.random() * leftItems.length - 1) + 1);
         nextItem = leftItems[randomNext];
+        for(var x = 0; x < tileContainer.children.length; x++){
+            if(tileContainer.children[x].className.slice(27, 28) >= 1){
+                tileContainer.children[x].className = defaultClasses[0];
+            }
+        }
         tileContainer.innerHTML += nextItem;
         leftItems = [];
         for(var j = 0; j < defaultTemplate.length; j++) {
